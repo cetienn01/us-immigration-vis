7bed2f6bed6785b05e9180c893d9811dac87cfac
seatuna <chibiynm@gmail.com>
2018-11-10 15:47:39 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+// SVG drawing area
+
+var margin = {top: 40, right: 10, bottom: 60, left: 60};
+
+var width = 700 - margin.left - margin.right,
+    height = 700 - margin.top - margin.bottom;
+
+var svg = d3.select("#chart-area").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
